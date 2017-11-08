a051ea21baa069ee07253e8679cd06ba9fb48ef7
“[heanuea]” <[G00318763@gmit.ie]>
2017-11-08 16:11:26 +0000

    add flask
    added flask

---
+++ README.md
@@ -0,0 +1,2 @@
+# Mnist-Digit-Reader-
+A Web Application Built with Flask and Python that reads images containing numbers with the Help of Tensor-flow should recognize each digit from 0 to 9  
---
+++ app.py
@@ -0,0 +1,49 @@
+import os
+from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+ 
+
+UPLOAD_FOLDER = '/path/to/the/uploads'
+ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/') 
+def main():
+
+    return render_template("index.html")
+
+
+@app.route("/", methods=['GET', 'POST']) 
+def upload_file():
+    if request.method == 'POST':
+        
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+       
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
+
+if __name__ == "__main__":
+    app.run() 
