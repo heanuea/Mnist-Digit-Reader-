d5ea6a299d374ff9b1370ee9b3e3e24330356116
heanuea <g00318763@gmit.ie>
2017-11-30 22:30:14 +0000

    add to app.py

--- app.py
+++ app.py
@@ -1,49 +1,48 @@
-import os
-from flask import Flask, flash, render_template, request, redirect, url_for, send_from_directory
-from werkzeug.utils import secure_filename
-
-
-UPLOAD_FOLDER = '/path/to/the/uploads'
-ALLOWED_EXTENSIONS = set(['pdf', 'png', 'jpg', 'jpeg', 'gif'])
+from flask import Flask, render_template, request
+from scipy.misc import imsave , imread, imresize
+import numpy as np
+import keras.models
+import re
+import base64
 
 
 app = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-
-@app.route('/') 
-def main():
-
+@app.route('/')
+def index():
     return render_template("index.html")
 
 
-@app.route("/", methods=['GET', 'POST']) 
-def upload_file():
-    if request.method == 'POST':
-        
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-       
+@app.route('/predict/', methods=['GET','POST'])
+def predict():
+    
+    parseImg(request.get_data())
 
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return
-@app.route('/uploads/<filename>')
-def uploaded_file(filename):
-    return send_from_directory(app.config['UPLOAD_FOLDER'],
-                               filename)
+    
+    x = imread('output.png', mode='L')
+    x = np.invert(x)
+    x = imresize(x,(28,28))
 
-if __name__ == "__main__":
-    app.run() 
+  
+    x = x.reshape(1,28,28,1)
+
+    model = models.load_model("model/mnist_model.h5.index")
+
+    
+    out = model.predict(x)
+    print(out)
+   
+    response = np.array_str(np.argmax(out, axis=1))
+    print(response)
+    return response
+
+
+def parseImg(imgData):
+  
+    imgstr = re.search(b'base64,(.*)', imgData).group(1)
+    with open('output.png','wb') as output:
+        output.write(base64.decodebytes(imgstr))
+
+if __name__ == '__main__':
+    app.run(debug = True)
